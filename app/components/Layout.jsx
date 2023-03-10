7e5184aa606c46baef720f93816e220e2f6df4e4
Dharti Tejani <designfordestiny@gmail.com>
2023-03-10 12:36:15 +0530

    Menu Chnages

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -238,6 +238,7 @@ function DesktopHeader({isHome, menu, openCart, title}) {
                 isActive ? 'pb-1 border-b -mb-px' : 'pb-1'
               }
             >
+              {item}
               {item.title}
             </Link>
           ))}
